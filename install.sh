abcd2a3ab15dabfab31cfacf16bc90127d186f29
Tim Dunn <me.timd1@gmail.com>
2019-07-09 09:55:34 -0400

    Improved install script
     - installs vim repos in loop
     - checks if directories exist before installation attempts
     - doesn't exit after installing fzf
     - echos what it's currently installing

--- install.sh
+++ install.sh
@@ -6,38 +6,52 @@ INSTALL_PATH=`pwd`
 ###############
 # INSTALL VIM #
 ###############
-sudo apt install vim
+echo "Installing and configuring vim..."
+sudo apt install -y vim curl sed
 
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# install packages
+# install vim plugins
 cd ~/.vim/bundle
-git clone https://github.com/christoomey/vim-tmux-navigator	# vim-tmux nav
-git clone https://github.com/tpope/vim-commentary	# easy commenting
-git clone https://github.com/tpope/vim-surround		# editing shortcuts
-git clone https://github.com/scrooloose/nerdtree	# tree fileviewer
-git clone https://github.com/sirver/ultisnips		# expansion shortcuts
-git clone https://github.com/tpope/vim-fugitive		# git integration
-git clone https://github.com/itchyny/lightline.vim	# aesthetics
-git clone https://github.com/tpope/vim-repeat		# plugin cmds now repeatable
-git clone https://github.com/w0rp/ale				# c syntax checker
-git clone https://github.com/junegunn/fzf.git		# fuzzy text finder
-git clone https://github.com/junegunn/fzf.vim.git	# fzf vim integration
-source fzf/install --no-fish --no-zsh
+VIM_PLUGIN_REPOS=(
+	christoomey/vim-tmux-navigator	# vim-tmux nav
+	tpope/vim-commentary			# easy commenting
+	tpope/vim-surround				# editing shortcuts
+	scrooloose/nerdtree				# tree fileviewer
+	sirver/ultisnips				# expansion shortcuts
+	tpope/vim-fugitive				# git integration
+	itchyny/lightline.vim			# aesthetics
+	tpope/vim-repeat				# plugin cmds now repeatable
+	w0rp/ale						# c syntax checker
+	junegunn/fzf					# fuzzy text finder
+	junegunn/fzf.vim				# fzf vim integration
+)
+for owner_repo in "${VIM_PLUGIN_REPOS[@]}"
+do
+	repo="$(cut -d "/" -f 2 <<< $owner_repo)"
+	[ ! -d "$repo" ] && git clone "https://github.com/$owner_repo.git"
+done
+
+# install fzf, then continue this script regardless of success
+sed -i -e 's/exit/return/g' fzf/install > fzf/install
+source fzf/install --no-fish --no-zsh || true
+
+# copy ultisnips snippets
 cd $INSTALL_PATH
 cp -r UltiSnips ~/.vim
 
 # set up custom vim colorscheme
-mkdir ~/.vim/colors
+[ ! -d "~/.vim/colors"] && mkdir ~/.vim/colors
 cp colorscheme.vim ~/.vim/colors/custom.vim
 
 
 ###############
 # INSTALL GIT #
 ###############
-sudo apt install git
+echo "Installing and configuring git..."
+sudo apt install -y git
 git config --global user.name "Tim Dunn"
 git config --global user.email "me.timd1@gmail.com"
 git config --global core.editor vim
@@ -50,19 +64,30 @@ git config --global merge.tool fugitive
 ################
 # INSTALL TMUX #
 ################
-sudo apt install tmux
+echo "Installing and configuring tmux..."
+sudo apt install -y tmux
 
 
 ##################
 # INSTALL PYTHON #
 ##################
-sudo apt install python python3 python-pip
+echo "Installing and configuring python..."
+sudo apt install -y python python3 python-pip
 sudo pip install numpy opencv-python
+
+
+###################
+# UPGRADE GDB/GEF #
+###################
+echo "Installing and configuring gdb/gef..."
+wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
+echo source ~/.gdbinit-gef.py >> ~/.gdbinit
 
 
 ###################
 # SET UP SSH KEYS #
 ###################
+echo "Setting up ssh keys..."
 if [ ! -f ~/.ssh/id_rsa.pub ]; then 
 	ssh-keygen -t rsa
 fi
@@ -72,8 +97,10 @@ fi
 ###################
 # CREATE DOTFILES #
 ###################
+echo "Installing all dotfiles..."
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
 cp fzf.bash ~/.fzf.bash
 cp tmux.conf ~/.tmux.conf
 source ~/.bashrc
+echo "Success!"
